dca11d9d0c02ac2d6ca7781989309ca2c16d1a5d
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-04-01 18:45:01 +0530

    Service-2 Added but not working

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ServiceSection } from "../data/mainData";
 import { Link } from "react-router-dom";
 import gsap from "gsap";
 
 const Service = () => {
+  const [index, setIndex] = useState(null);
   useEffect(() => {
     gsap.to(".main-text h2", {
       x: 0,
@@ -12,7 +13,7 @@ const Service = () => {
       ease: "power3.out",
 
       scrollTrigger: {
-        trigger: ".Services",
+        trigger: ".Service-1",
         start: "top 90%",
         end: "bottom 80%",
         // markers: true,
@@ -50,6 +51,26 @@ const Service = () => {
             <span>{ServiceSection[1].button}</span>
             <span>{ServiceSection[1].button}</span>
           </Link>
+        </div>
+      </div>
+
+      <div className="Service-2">
+        <div className="text-container">
+          {ServiceSection[2].map((elem, index) => (
+            <div
+              className="text-card"
+              key={index}
+              onClick={() => setIndex(index)}
+            >
+              <h3>{elem.text}</h3>
+              <p>{elem.desc}</p>
+            </div>
+          ))}
+        </div>
+        <div className="image-container">
+          {ServiceSection[2].map((elem, index) => (
+            <img src={elem.image} alt={elem.text} key={index} />
+          ))}
         </div>
       </div>
     </div>
--- src/data/mainData.jsx
+++ src/data/mainData.jsx
@@ -46,6 +46,32 @@ const ServiceSection = [
     button: "read more",
     link: "#",
   },
+  [
+    {
+      text: "Transparent",
+      desc: "We give you an overview on thousands of data providers. You get the full picture.",
+      image:
+        "https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    },
+    {
+      text: "Convenient",
+      desc: "Preview data samples instantly and find pricing information that help you to benchmark.",
+      image:
+        "https://images.unsplash.com/photo-1640158615573-cd28feb1bf4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      text: "Helpful",
+      desc: "Get complimentary sourcing advice by our team of data acquisition specialists.",
+      image:
+        "https://images.unsplash.com/photo-1532619675605-1ede6c2ed2b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+      text: "Free-of-charge",
+      desc: "Our service is free for you. We’re paid by the provider in case you make a purchase.",
+      image:
+        "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+  ],
 ];
 
 export { logoName, menuNavigationLinks, heroSection, ServiceSection };
